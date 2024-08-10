a9f298ae735d46cb8ce2029e96d9940871efe039
AliGhalenoei <alighalenoei8383@gmail.com>
2024-08-10 23:04:44 +0330

    add comments and contact us (e)

--- main/options/urls.py
+++ main/options/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('storys/',views.StorysAPIView.as_view(),name='storys'),
     
     # comment products
-    path('comment/products/<slug:product_id>/',views.CommentProductsAPIView.as_view(),name = 'comment_products'),
+    path('comment/products/<int:product_id>/',views.CommentProductsAPIView.as_view(),name = 'comment_products'),
     path('add/comment/<int:product_id>/',views.CreateCommentAPIView.as_view(),name='add_comment'),
     path('update/comment/<int:comment_id>/',views.UpdateCommentProductAPIView.as_view(),name='update_comment'),
     path('delete/comment/<int:comment_id>/',views.DeleteCommentProductAPIView.as_view(),name='delete_comment'),
