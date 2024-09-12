e0bd883cf50aa2509d191562d4f93c110bfe52bc
AliGhalenoei <alighalenoei8383@gmail.com>
2024-09-13 00:46:22 +0330

    edit menus

--- main/panel/models.py
+++ main/panel/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class MenuNavbar(models.Model):
     title = models.CharField(max_length=250)
     link = models.CharField(max_length=250)
-
+    sub_menu = models.BooleanField(default=False)
     def __str__(self) -> str:
         return self.title
     
